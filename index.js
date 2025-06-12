83ac00299f418fd31022b7206f3c1ed9bf806214
Benadette Mokandu <benadettemokandu@gmail.com>
2025-06-12 09:09:22 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup,hq){
+    return Math.abs(hq - pickup);
+}
+
+function distanceFromHqInFeet(pickup,hq){
+    return distanceFromHqInBlocks(pickup,hq)*264;
+}
+function calculateFarePrices(pickup,hq){
+    const distance= distanceFromHqInFeet(pickup,hq);
+    if (distance <=400){
+        return 0;
+    } else if (distance >400 && <= 2000) {
+        return (distance - 400)*0.02
+    } else if (distance > 2000 && < 2500) {
+        return 25
+    }
+    else if (distance > 2500) {
+        return "cannot travel that far";
+    }
+    
+}
